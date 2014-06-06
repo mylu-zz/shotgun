6efbd14c6c96f36823920a201698d1bcd0370be4
Max Lu <malu@malu-mn1.linkedin.biz>
2014-06-05 21:14:40 -0700

    woops

--- sketch.py
+++ sketch.py
@@ -3,12 +3,23 @@ import json
 #import numpy as np
 #import sklearn
 import csv
+import urllib2
 #from sklearn import svm
 #from sklearn import tree
 #from sklearn import linear_model
 
 symbol = 'goog'
+url = 'http://ichart.yahoo.com/table.csv?s=' + symbol + '&a=5&b=1&c=2009 &d=0&e=31&f=2010&g=w&ignore=.csv'
+urllib2.open(url);
+subprocess.call('mv ~/Downloads/table.csv .',shell=True)
+stock_info = []
 with open(symbol + '.csv', 'rb') as csvfile:
 	reader = csv.reader(csvfile, delimiter= ',')
 	for row in reader:
-		print ', '.join(row)
+		stock_info.append(row)
+
+stock_info = np.matrix(stock_info)
+
+
+
+
