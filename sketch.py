2c7a13730200d6109f095947cee713c76df3391f
Johan Brook <johan.jvb@gmail.com>
2014-06-05 22:11:45 -0700

    fixed bugs in csv.reader, blah matrix should be properly formatted

--- sketch.py
+++ sketch.py
@@ -5,6 +5,7 @@ import numpy as np
 import csv
 import urllib2
 import subprocess
+import StringIO
 #from sklearn import svm
 #from sklearn import tree
 #from sklearn import linear_model
@@ -13,14 +14,15 @@ symbol = 'goog'
 url = 'http://ichart.yahoo.com/table.csv?s=' + symbol #+ '&a=5&b=1&c=2009 &d=0&e=31&f=2010&g=w&ignore=.csv'
 q = urllib2.urlopen(url)
 csvfile = q.read()
+f = StringIO.StringIO(csvfile)
 #subprocess.call('mv ~/Downloads/table.csv .',shell=True)
 stock_info = []
 #with open(symbol + '.csv', 'rb') as csvfile:
-reader = csv.reader(csvfile, delimiter= ',')
+reader = csv.reader(f, delimiter=",")
 for row in reader:
 	stock_info.append(row)
 
-stock_info = np.matrix(stock_info)
+blah = np.matrix(stock_info)
 
 
 
