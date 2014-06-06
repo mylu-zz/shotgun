78166cb93bfe400e73101cbb4dec365e0c6e2d66
Johan Brook <johan.jvb@gmail.com>
2014-06-05 21:52:19 -0700

    fixed sketch.py bugs, should run with command {python sketch.py}

--- sketch.py
+++ sketch.py
@@ -1,22 +1,24 @@
 import json
 #import scipy as sp
-#import numpy as np
+import numpy as np
 #import sklearn
 import csv
 import urllib2
+import subprocess
 #from sklearn import svm
 #from sklearn import tree
 #from sklearn import linear_model
 
 symbol = 'goog'
-url = 'http://ichart.yahoo.com/table.csv?s=' + symbol + '&a=5&b=1&c=2009 &d=0&e=31&f=2010&g=w&ignore=.csv'
-urllib2.open(url);
-subprocess.call('mv ~/Downloads/table.csv .',shell=True)
+url = 'http://ichart.yahoo.com/table.csv?s=' + symbol #+ '&a=5&b=1&c=2009 &d=0&e=31&f=2010&g=w&ignore=.csv'
+q = urllib2.urlopen(url)
+csvfile = q.read()
+#subprocess.call('mv ~/Downloads/table.csv .',shell=True)
 stock_info = []
-with open(symbol + '.csv', 'rb') as csvfile:
-	reader = csv.reader(csvfile, delimiter= ',')
-	for row in reader:
-		stock_info.append(row)
+#with open(symbol + '.csv', 'rb') as csvfile:
+reader = csv.reader(csvfile, delimiter= ',')
+for row in reader:
+	stock_info.append(row)
 
 stock_info = np.matrix(stock_info)
 
