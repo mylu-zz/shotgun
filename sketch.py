867291161b8c58c8d298d95a367ce41f37842910
Johan Brook <johan.jvb@gmail.com>
2014-06-06 21:11:46 -0700

    added comment for describing how to retrieve full amount of data from YQL, including descriptions for parameters + appropriate URL

--- sketch.py
+++ sketch.py
@@ -11,6 +11,26 @@ import StringIO
 #from sklearn import linear_model
 
 symbol = 'goog'
+
+"""
+http://ichart.finance.yahoo.com/table.csv?s=GE&a=00&b=2&c=1962&d=05&e=10&f=2014&g=d&ignore=.csv
+
+Meaning of parameters 
+	- note the dates do not have to be within actual range of company lifetime
+	- for example, doing Facebook (sym = FB) between 1/2/1962 - 5/10/2014 will return prices between 5/18/2012 - 5/9/2014. 
+a = start month (0 indexed, starting january = 00)
+b = start day (1 indexed, starting with the first day of month)
+c = start year
+d = end month
+e = end day
+f = end year
+g = type of request (time interval)
+	d -> Daily
+	w -> Weekly
+	m -> Monthly
+	v -> Dividends only
+"""
+
 url = 'http://ichart.yahoo.com/table.csv?s=' + symbol #+ '&a=5&b=1&c=2009 &d=0&e=31&f=2010&g=w&ignore=.csv'
 q = urllib2.urlopen(url)
 csvfile = q.read()
@@ -23,7 +43,7 @@ for row in reader:
 	stock_info.append(row)
 
 blah = np.matrix(stock_info)
-
+blah = blah[1:,1:]
 
 
 
