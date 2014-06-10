7e547966e5e0355347a4f092589d72d73e2b621a
Max Lu <malu@malu-mn1.linkedin.biz>
2014-06-09 22:29:30 -0700

    momentums

--- sketch.py
+++ sketch.py
@@ -6,11 +6,12 @@ import csv
 import urllib2
 import subprocess
 import StringIO
+import sys
 #from sklearn import svm
 #from sklearn import tree
 #from sklearn import linear_model
 
-symbol = 'msft'
+symbol = sys.argv[1]
 
 """
 Note that Google for some reason only returns prices for 3/27/2014 - present, not full IPO length? Why
@@ -44,9 +45,29 @@ reader = csv.reader(f, delimiter=",")
 for row in reader:
 	stock_info.append(row)
 
-blah = np.matrix(stock_info)
-blah = blah[1:,1:]
+stock_info = np.matrix(stock_info)
+# Percent changes in OPENING price per day
+open_price = [float(x.tolist()[0][1]) for x in stock_info[1:len(stock_info)]]
+percent_change_open = [np.divide((x - open_price[i-1]), open_price[i - 1]) for i, x in enumerate(open_price)][1:]
+percent_change_open = np.matrix(['PercChangeOpen1Day',np.nan]+percent_change_open).T
+# add as another feature column
+stock_info = np.append(stock_info, percent_change_open, axis=1)
 
-print blah
 
+#Percent changes in OPENING price per 5 days
+percent_change_5_open = [np.divide((x - open_price[i-5]), open_price[i - 5]) for i, x in enumerate(open_price)][5:]
+percent_change_5_open = np.matrix(['PercChangeOpen5Day',np.nan,np.nan,np.nan,np.nan,np.nan]+percent_change_5_open).T
+# add as another feature column
+stock_info = np.append(stock_info, percent_change_5_open, axis=1)
+
+#I(close > open) on a given day where I is indicator fn
+close_price = [float(x.tolist()[0][4]) for x in stock_info[1:len(stock_info)]]
+indic = [1 if close_price[i]>open_price[i] else 0 for i in range(len(close_price))]
+indic = np.matrix(['I(Close>Open)']+indic).T
+# add as another feature column
+stock_info = np.append(stock_info, indic, axis=1)
+
+print stock_info
+
+#run svm
 
